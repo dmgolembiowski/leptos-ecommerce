3f7857a31f9737db7adba0a96042cbc6960e36db
dmgolembiowski <dmgolembiowski@gmail.com>
2024-06-09 09:25:38 -0400

    Tidy

--- server/src/state.rs
+++ server/src/state.rs
@@ -1,18 +1,35 @@
-#![allow(unused_imports, unused_variables)]
-use axum::extract::FromRef;
-use common::{
-    BillLineItems, BillRow, BillRowId, Count, CustomerId, CustomerRow, Inventory, InventoryName,
-    InventoryRow, InventoryRowId, Money, Time,
+#![allow(unused_import_braces, unused_imports)]
+#[rustfmt::skip]
+use {
+    axum::extract::FromRef,
+    common::{
+        BillLineItems, 
+        Count, 
+        CustomerId, 
+        Inventory, 
+        InventoryName,
+        InventoryRow, 
+        InventoryRowId, 
+        Money, 
+    },
+    leptos::{
+        html::q, 
+        prelude::LeptosOptions,
+    },
+    leptos_axum::AxumRouteListing,
+    rusqlite::Connection,
+    std::{
+        collections::BTreeMap, 
+        sync::Arc, 
+        vec::IntoIter,
+    },
+    tokio::sync::Mutex,
+    tokio_stream::{
+        Iter as StreamIter, 
+        StreamExt, 
+        StreamMap,
+    },
 };
-use leptos::{html::q, prelude::LeptosOptions};
-use leptos_axum::AxumRouteListing;
-use rusqlite::Row;
-use rusqlite::{prepare_and_bind, Connection};
-use std::vec::IntoIter;
-use std::{borrow::Borrow, collections::BTreeMap, sync::Arc};
-use tokio::sync::Mutex;
-use tokio_stream::{Iter as StreamIter, StreamExt, StreamMap};
-
 /// This takes advantage of Axum's SubStates feature by deriving FromRef. This is the only way to have more than one
 /// item in Axum's State. Leptos requires you to have leptosOptions in your State struct for the leptos route handlers
 #[derive(FromRef, Debug, Clone)]
