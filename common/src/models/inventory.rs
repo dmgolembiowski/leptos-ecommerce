880adfe004d6acb070303db12d2a2a85e33b0860
dmgolembiowski <dmgolembiowski@gmail.com>
2024-06-11 13:43:57 -0400

    Prepares testing branch

--- common/src/models/inventory.rs
+++ common/src/models/inventory.rs
@@ -1,8 +1,8 @@
 use crate::models::types::*;
-use derive_builder::Builder;
-use std::path::PathBuf;
 use cfg_if::cfg_if;
+use derive_builder::Builder;
 use errors::EcommerceAppError;
+use std::path::PathBuf;
 
 /// By default, the server will use this type
 /// to implement the `Inventory` trait, however
@@ -21,36 +21,57 @@ pub struct InventoryRow {
 
 pub struct Inventory(Vec<InventoryRow>);
 
+impl Inventory {
+    pub fn rows(&self) -> &Vec<InventoryRow> {
+        &self.0
+    }
+}
+
+#[rustfmt::skip]
 cfg_if! {
-if #[cfg(feature = "ssr")] {
+    if #[cfg(feature = "ssr")] {
         use crate::conn;
-impl Inventory{
-        pub async fn get() -> Result<Self, EcommerceAppError> {
-        let conn_raw = conn().await?;
-        let conn = &mut *conn_raw.lock().await;
-        let mut stmt = conn.prepare("SELECT id, name, asset, cost, quantity_available, created_at, updated_at FROM inventory").unwrap();
-        let mut rows = stmt.query([]).unwrap();
-        let mut it: Vec<InventoryRow> = vec![];
-        while let Some(row) = rows.next().unwrap() {
-            it.push(
-                crate::InventoryRowBuilder::default()
-                    .id(row.get(0).unwrap())
-                    .name(row.get(1).unwrap())
-                    .asset(<String as Into<std::path::PathBuf>>::into(
-                        row.get(2).unwrap(),
-                    ))
-                    .cost(row.get(3).unwrap())
-                    .quantity_available(row.get(4).unwrap())
-                    .created_at(row.get(5).unwrap())
-                    .updated_at(row.get(6).unwrap())
-                    .build()
-                    .unwrap(),
-            );
+
+        impl Inventory{
+                pub async fn get() -> Result<Self, EcommerceAppError> {
+                let conn_raw = conn().await?;
+                let conn = &mut *conn_raw.lock().await;
+                let mut stmt = conn.prepare("SELECT id, name, asset, cost, quantity_available, created_at, updated_at FROM inventory").unwrap();
+                let mut rows = stmt.query([]).unwrap();
+                let mut it: Vec<InventoryRow> = vec![];
+                while let Some(row) = rows.next().unwrap() {
+                    it.push(
+                        crate::InventoryRowBuilder::default()
+                            .id(row.get(0).unwrap())
+                            .name(row.get(1).unwrap())
+                            .asset(<String as Into<std::path::PathBuf>>::into(
+                                row.get(2).unwrap(),
+                            ))
+                            .cost(row.get(3).unwrap())
+                            .quantity_available(row.get(4).unwrap())
+                            .created_at(row.get(5).unwrap())
+                            .updated_at(row.get(6).unwrap())
+                            .build()
+                            .unwrap(),
+                    );
+                }
+                Ok(Inventory(it))
+            }
         }
-        Ok(Inventory(it))
     }
-
-
 }
-}
+
+#[cfg(feature = "ssr")]
+#[cfg(test)]
+mod tests {
+    use super::Inventory;
+    use tokio;
+
+    #[tokio::test]
+    async fn test_inventory_retrieval() {
+        let res = async { Inventory::get().await }.await;
+        assert!(res.is_ok());
+        let res = res.unwrap();
+        assert!(res.rows().len() > 0, "expected more than 0 rows");
     }
+}
