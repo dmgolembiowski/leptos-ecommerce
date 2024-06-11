e6e417010bd38a7d62084e1a5bd5f68b4c5bc72f
dmgolembiowski <dmgolembiowski@gmail.com>
2024-06-11 17:29:25 -0400

    Fixing damaged Inventory impl

--- common/src/models/inventory.rs
+++ common/src/models/inventory.rs
@@ -24,39 +24,46 @@ pub struct InventoryRow {
 pub struct Inventory(Vec<InventoryRow>);
 
 impl Inventory {
-    cfg_if! {
-        if #[cfg(feature = "ssr")] {
-            use crate::conn;
+    pub fn rows(&self) -> &Vec<InventoryRow> {
+        &self.0
+    }
+    pub fn into_inner(self) -> Vec<InventoryRow> {
+        self.0
+    }
+}
 
-            impl Inventory{
-                    pub async fn get() -> Result<Self, EcommerceAppError> {
-                    let conn_raw = conn().await?;
-                    let conn = &mut *conn_raw.lock().await;
-                    let mut stmt = conn.prepare("SELECT id, name, asset, cost, quantity_available, created_at, updated_at FROM inventory").unwrap();
-                    let mut rows = stmt.query([]).unwrap();
-                    let mut it: Vec<InventoryRow> = vec![];
-                    while let Some(row) = rows.next().unwrap() {
-                        it.push(
-                            crate::InventoryRowBuilder::default()
-                                .id(row.get(0).unwrap())
-                                .name(row.get(1).unwrap())
-                                .asset(<String as Into<std::path::PathBuf>>::into(
-                                    row.get(2).unwrap(),
-                                ))
-                                .cost(row.get(3).unwrap())
-                                .quantity_available(row.get(4).unwrap())
-                                .created_at(row.get(5).unwrap())
-                                .updated_at(row.get(6).unwrap())
-                                .build()
-                                .unwrap(),
-                        );
-                    }
-                    Ok(Inventory(it))
+cfg_if! {
+    if #[cfg(feature = "ssr")] {
+
+        impl Inventory{
+                pub async fn get() -> Result<Self, EcommerceAppError> {
+                let conn_raw = crate::conn().await?;
+                let conn = &mut *conn_raw.lock().await;
+                let mut stmt = conn.prepare("SELECT id, name, asset, cost, quantity_available, created_at, updated_at FROM inventory").unwrap();
+                let mut rows = stmt.query([]).unwrap();
+                let mut it: Vec<InventoryRow> = vec![];
+                while let Some(row) = rows.next().unwrap() {
+                    it.push(
+                        crate::InventoryRowBuilder::default()
+                            .id(row.get(0).unwrap())
+                            .name(row.get(1).unwrap())
+                            .asset(<String as Into<std::path::PathBuf>>::into(
+                                row.get(2).unwrap(),
+                            ))
+                            .cost(row.get(3).unwrap())
+                            .quantity_available(row.get(4).unwrap())
+                            .created_at(row.get(5).unwrap())
+                            .updated_at(row.get(6).unwrap())
+                            .build()
+                            .unwrap(),
+                    );
                 }
+                Ok(Inventory(it))
             }
         }
     }
 }
+
 #[cfg(feature = "ssr")]
 #[cfg(test)]
 mod tests {
