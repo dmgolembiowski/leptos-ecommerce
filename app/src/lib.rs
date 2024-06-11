f8692446efa92a957c350ffb522fd7071248fd24
benwis <ben@celcyon.com>
2024-06-10 22:17:52 -0700

    Some fixes, not quite complete

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,8 +1,6 @@
 #![allow(non_snake_case)]
 use leptos::prelude::*;
-use leptos::*;
 use leptos_meta::*;
-use leptos_reactive::{create_signal, *};
 use leptos_router::{
     components::{FlatRoutes, Route, Router},
     StaticSegment,
@@ -55,11 +53,14 @@ fn HomePage() -> impl IntoView {
 
 #[component]
 pub fn Catalog() -> impl IntoView {
-    let inventory_row_items = create_resource(move || functions::inventory::catalog::get_catalog());
-    let (pending, set_pending) = create_signal(false);
+    let inventory_row_items = Resource::new_serde(
+        || (),
+        move |_| async move {functions::inventory::catalog::get_catalog().await},
+    );
+    let (pending, set_pending) = signal(false);
     let hide_more_cookies = move || {
         pending.get()
-            || inventory_row_items
+            || inventory_row_items.get()
                 .map(|cookie_vec| {
                     cookie_vec
                         .as_ref()
@@ -69,37 +70,43 @@ pub fn Catalog() -> impl IntoView {
                 .unwrap_or_default()
     };
     view! {
-        {move || if hide_more_cookies() {
-             view! {
-                <div> "Loading..." </div>
-             }
-        }
-        else {
-            view! {
-                <div>
-                    <Transition
-                        fallback=|| ()
-                        set_pending
-                    >
-                        {move || inventory_row_items.get().map(|cookie_row| cookie_row.map(|row| view! {
-                            <ul>
-                                <For
-                                    each=move || row.clone()
-                                    key=|r| r.id
-                                    let:r
-                                >
-                                    <li>
-                                        <span>{r.name}</span>
-                                        <span>{r.price}</span>
-                                        <span>{r.asset}</span>
-                                        <span>{r.cost}</span>
-                                        <span>{r.quantity_available}</span>
-                                    </li>
-                                </For>
-                            </ul>
-                        }))}
-                    </Transition>
-                </div>
+        {move || {
+            if hide_more_cookies() {
+                view! { <div>"Loading..."</div> }.into_view()
+            } else {
+                view! {
+                    <div>
+                        <Transition fallback=|| {
+                            set_pending().set(|| true);
+                            ().into_view()
+                        }>
+                            {move || {
+                                inventory_row_items
+                                    .get()
+                                    .map(|cookie_row| {
+                                        cookie_row
+                                            .map(|row| {
+                                                view! {
+                                                    <ul>
+                                                        <For each=move || row.clone() key=|r| r.id let:r>
+                                                            <li>
+                                                                <span>{r.name}</span>
+                                                                <span>{r.price}</span>
+                                                                <span>{r.asset}</span>
+                                                                <span>{r.cost}</span>
+                                                                <span>{r.quantity_available}</span>
+                                                            </li>
+                                                        </For>
+                                                    </ul>
+                                                }
+                                            })
+                                    })
+                            }}
+
+                        </Transition>
+                    </div>
+                }
+                    .into_view()
             }
         }}
     }
